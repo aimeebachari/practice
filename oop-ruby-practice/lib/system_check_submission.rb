f5e131a56917944409ae39bcfe6428ead3e95c56
Aimee Bachari <aimeebachari@gmail.com>
2017-03-15 12:05:52 -0400

    added assign_grade method to System Check Submission class

--- oop-ruby-practice/lib/system_check_submission.rb
+++ oop-ruby-practice/lib/system_check_submission.rb
@@ -1,9 +1,20 @@
 class SystemCheckSubmission
-  attr_reader :solution, :student
+  include Grade
+
+  attr_reader :solution, :student, :grade
 
   def initialize(solution, student)
     @solution = solution
     @student = student
+    @grade = nil
+  end
+
+  def assign_grade(grade)
+    if GRADES.include?(grade)
+      @grade = grade
+    else
+      raise InvalidGradeError
+    end
   end
 end
 
--- oop-ruby-practice/spec/lib/07_exceeds_expectations_cohort_spec.rb
+++ oop-ruby-practice/spec/lib/07_exceeds_expectations_cohort_spec.rb
@@ -17,7 +17,7 @@ describe Cohort do
     cohort
   end
 
-  xdescribe "#system_check_completed?" do
+  describe "#system_check_completed?" do
     it "returns 'true' if everyone has submitted" do
       jane = cohort_with_students.students.first
       john = cohort_with_students.students[1]
