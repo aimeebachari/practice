c68cb0478a439cc802f2099d736f58e1cdf0535b
Aimee Bachari <aimeebachari@gmail.com>
2017-03-15 11:09:14 -0400

    initialized variables and added enroll method to Cohort class

--- oop-ruby-practice/lib/cohort.rb
+++ oop-ruby-practice/lib/cohort.rb
@@ -1,9 +1,16 @@
 class Cohort
-  attr_reader :title, :start_date, :end_date
+  attr_reader :title, :start_date, :end_date, :students, :system_checks, :career_kickoff
 
   def initialize(title, start_date, end_date)
     @title = title
     @start_date = start_date
     @end_date = end_date
+    @students = []
+    @system_checks = []
+    @career_kickoff = @end_date + 4
+  end
+
+  def enroll(student)
+    @students << student unless @students.include?(student)
   end
 end
