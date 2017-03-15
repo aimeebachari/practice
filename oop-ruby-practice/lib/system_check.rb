6645395af1250e05cb5f57053b4be3ba0998bcf0
Aimee Bachari <aimeebachari@gmail.com>
2017-03-15 10:41:34 -0400

    added arguments, reader for submissions, and add_submission method to System Check class

--- oop-ruby-practice/lib/system_check.rb
+++ oop-ruby-practice/lib/system_check.rb
@@ -1,2 +1,11 @@
 class SystemCheck
+  attr_reader :submissions
+
+  def initialize(name, due)
+    @submissions = []
+  end
+
+  def add_submission(submission)
+    @submissions << submission
+  end
 end
