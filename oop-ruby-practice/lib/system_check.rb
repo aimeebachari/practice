2df2fc48f4b862e484de8d403de3fa443b2d45f2
Aimee Bachari <aimeebachari@gmail.com>
2017-03-15 12:16:29 -0400

    added average_grade method to system check class

--- oop-ruby-practice/lib/system_check.rb
+++ oop-ruby-practice/lib/system_check.rb
@@ -1,11 +1,20 @@
 class SystemCheck
-  attr_reader :submissions
+  attr_reader :submissions, :average
 
   def initialize(name, due)
     @submissions = []
+    @average = nil
   end
 
   def add_submission(submission)
     @submissions << submission
+  end
+
+  def average_grade
+    sum = 0
+    @submissions.each do |submission|
+      sum += submission.grade
+    end
+    @average = sum.to_f / @submissions.length
   end
 end
