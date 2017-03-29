c657ad8d3793610895d1ecc99904e872821086ba
Aimee Bachari <aimeebachari@gmail.com>
2017-03-29 09:53:56 -0400

    Created migration for projects and updated schema.

--- project-planner-online/db/schema.rb
+++ project-planner-online/db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329133140) do
+ActiveRecord::Schema.define(version: 20170329135048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name",        null: false
+    t.string "description"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name",  null: false
